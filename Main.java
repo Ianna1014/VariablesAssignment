d6d79afb10cbf75412f362cd7542df6e2659bce3
Ianna1014 <43189854+Ianna1014@users.noreply.github.com>
2020-08-31 16:23:08 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int t = 50;
+	String username = "ianna1014";
+	float price = 24.99f;
+	System.out.println("Test Score initialized is: " + t );
+	System.out.println("Price tag: " + price);
+	System.out.println("Username: " + username);
+    }
+}
